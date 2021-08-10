b80479ae5f670ad2ea9ff7df140184df1a543343
Shachi1 <mehjabeen.shachi@gmail.com>
2021-08-11 02:23:13 +0600

    separate home and contact function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module practice
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -3,30 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>Welcome world!</h1>")
+// 	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/"{
+// 		fmt.Fprint(w, "Email me at: <a href=\"mailtoabc@gmail.com\">abc@gmail.com</a>")
+// 	} else {
+// 		w.WriteHeader(404)
+// 		fmt.Fprint(w, "<p>please email us if you keep being sent to the invalid page</p>")
+// 	}
+// }
+
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "Email me at: <a href=\"mailtoabc@gmail.com\">abc@gmail.com</a>")
-	} else {
-		w.WriteHeader(404)
-		fmt.Fprint(w, "<p>please email us if you keep being sent to the invalid page</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome FAQ!</h1>")
 }
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Email me at: <a href=\"mailtoabc@gmail.com\">abc@gmail.com</a>")
+}
+
 func main() {
 
-	// var name string
-	// fmt.Println("What is your name?")
-	// // fmt.Scanf("%s", &name)
-	// fmt.Println(name)
-
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handlerFunc) 
+	r := mux.NewRouter()
+	// r.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+	// mux := &http.ServeMux{}
+	// mux.HandleFunc("/", handlerFunc)
 	//http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", mux)
+	// http.ListenAndServe(":3000", mux)
 	// http.ListenAndServe(":3000", nil)
 
 }
